0d00c94ac5dd9ccea5165740f2c355cbb739985e
Less Wright <lessw@etrillium.com>
2020-01-28 10:16:32 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,9 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1Xg9qs-fz3hpZORYDMSq4qGY8-yR6m_SL/view?usp=sharing'
-export_file_name = 'EffNetB5-Flu100'
+export_file_url = 'https://b5-100-1.s3-us-west-2.amazonaws.com'
+#'https://drive.google.com/file/d/1Xg9qs-fz3hpZORYDMSq4qGY8-yR6m_SL/view?usp=sharing'
+export_file_name = 'export.pkl'
 
 classes = ['both', 'flu-a', 'flu-b', 'invalid', 'negative']
 path = Path(__file__).parent
